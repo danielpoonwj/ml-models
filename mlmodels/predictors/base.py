da6d0619c0343314ef549ab24f1978dd1030f86c
Daniel Poon <daniel.poon.wenjie@gmail.com>
2017-11-12 21:37:03 +0800

    Implement alternative mode to save/load from local disk

--- mlmodels/predictors/base.py
+++ mlmodels/predictors/base.py
@@ -26,7 +26,6 @@ class BasePredictor:
     def __init__(self, prediction_field, version=1):
         self.predictor_name = camel_to_snake(self.__class__.__name__)
         self.version = version
-        self.bucket = os.environ['AWS_BUCKET_NAME']
 
         self.prediction_field = prediction_field
         self.ignore_columns = set()
@@ -36,10 +35,23 @@ class BasePredictor:
         self.clf = None
         self.clf_version = None
 
-        self.s3_prefix = '{predictor_name}/v{version}'.format(
+        self.folder_prefix = '{predictor_name}/v{version}'.format(
             predictor_name=self.predictor_name,
             version=self.version
         )
+
+        self._save_mode = os.environ.get('ML_MODELS_MODE', 'local')
+        assert self._save_mode in ('local', 'aws')
+
+        # validation for necessary aws environment variables
+        if self._save_mode == 'aws':
+            for env_var_name in ('AWS_ACCESS_KEY_ID', 'AWS_SECRET_ACCESS_KEY', 'AWS_BUCKET_NAME'):
+                env_var_val = os.environ.get(env_var_name)
+                assert env_var_val is not None and len(env_var_val) > 0, \
+                    '%s has to be set if mode=aws' % env_var_name
+
+        # unused if save mode is local
+        self.bucket = os.environ.get('AWS_BUCKET_NAME')
 
     def add_ignore_column(self, column_name):
         self.ignore_columns.add(column_name)
@@ -64,11 +76,32 @@ class BasePredictor:
 
         return data
 
-    def save(self):
-        """
-        Save trained timestamped model
-        """
+    def _local_dir(self):
+        models_dir = os.environ.get('ML_MODELS_DIR', '/tmp/ml_models')
+        write_dir = os.path.join(models_dir, self.folder_prefix)
+        if not os.path.exists(write_dir):
+            os.makedirs(write_dir)
 
+        return write_dir
+
+    def save(self):
+        getattr(self, '_save_%s' % self._save_mode)()
+
+    def _save_local(self):
+        assert isinstance(self.clf, BaseEstimator)
+        current_timestamp = int(datetime.timestamp(datetime.now()))
+
+        write_path = os.path.join(self._local_dir(), '%s.pkl' % current_timestamp)
+        joblib.dump(self.clf, write_path)
+
+        self.clf_version = '{folder_prefix}/{timestamp}'.format(
+            folder_prefix=self.folder_prefix,
+            timestamp=current_timestamp
+        )
+
+        print('Model saved to local path: %s' % write_path)
+
+    def _save_aws(self):
         assert isinstance(self.clf, BaseEstimator)
 
         file_buffer = BytesIO()
@@ -77,8 +110,8 @@ class BasePredictor:
 
         s3 = boto3.resource('s3')
 
-        self.clf_version = '{s3_prefix}/{timestamp}'.format(
-            s3_prefix=self.s3_prefix,
+        self.clf_version = '{folder_prefix}/{timestamp}'.format(
+            folder_prefix=self.folder_prefix,
             timestamp=int(datetime.timestamp(datetime.now()))
         )
 
@@ -89,27 +122,46 @@ class BasePredictor:
             Body=file_buffer
         )
 
-    def get_latest_key(self, to_underscore=False):
+        print('Model saved to S3 object: %s' % key_name)
+
+    def get_latest(self):
+        return getattr(self, '_get_latest_%s' % self._save_mode)()
+
+    def _get_latest_local(self):
+        file_names = os.listdir(self._local_dir())
+        file_names.sort()
+        
+        return file_names.pop()
+
+    def _get_latest_aws(self):
         s3 = boto3.client('s3')
 
         object_list = s3.list_objects_v2(
             Bucket=self.bucket,
-            Prefix=self.s3_prefix
+            Prefix=self.folder_prefix
         ).get('Contents', [])
 
         object_list.sort(key=lambda x: x.get('Key'))
         object_key = object_list.pop()['Key']
 
-        if to_underscore:
-            object_key = object_key.replace('/', '_')
-
         return object_key
 
     def load(self):
+        getattr(self, '_load_%s' % self._save_mode)()
+    
+    def _load_local(self):
+        latest_file = self._get_latest_local()
+
+        read_path = os.path.join(self._local_dir(), latest_file)
+        self.clf = joblib.load(read_path)
+        self.clf_version = '%s/%s' % (self.folder_prefix, latest_file.replace('.pkl', ''))
+
+        print('Model loaded from local path: %s' % read_path)
+    
+    def _load_aws(self):
         s3 = boto3.client('s3')
 
-        object_key = self.get_latest_key()
-        self.clf_version = object_key.replace('.pkl', '')
+        object_key = self._get_latest_aws()
 
         resp = s3.get_object(
             Bucket=self.bucket,
@@ -118,6 +170,9 @@ class BasePredictor:
 
         pickle_buffer = BytesIO(resp['Body'].read())
         self.clf = joblib.load(pickle_buffer)
+        self.clf_version = object_key.replace('.pkl', '')
+
+        print('Model loaded from S3 object: %s' % object_key)
 
     def predict(self, entry):
         assert isinstance(entry, dict)
